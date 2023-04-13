19906a6cc6349a991fdadeadf605b04f5440c751
Amy Li <amyamay22@gmail.com>
2023-04-13 17:31:58 -0400

    Configure and format calendar

--- js/fullcalendar_view.js
+++ js/fullcalendar_view.js
@@ -13,9 +13,61 @@
 
         var calendar = new FullCalendar.Calendar(calendarEl, {
           initialView: 'multiMonthYear',
-          events: api_endpoint,
-          navLinks: true,
-          eventLimit: true,
+          //events: api_endpoint,
+          events: [
+            {
+              start: '2023-04-01',
+              display: "background"
+            },
+            {
+              start: '2023-04-07',
+              display: "background"
+            },
+            {
+              start: '2023-04-09',
+              display: "background"
+            },
+            {
+              start: '2023-04-16',
+              display: "background"
+            },
+            {
+              start: '2023-04-11',
+              display: "background"
+            },
+            {
+              start: '2023-04-25',display: "background"
+            },
+            {
+              start: '2023-04-29',display: "background"
+            },
+            {
+              start: '2023-04-26',display: "background"
+            },
+            {
+              start: '2023-04-24',display: "background"
+            },
+            {
+              start: '2023-04-10',display: "background"
+            },
+            {
+              start: '2023-04-13',display: "background"
+            },
+            {
+              start: '2023-04-28',display: "background"
+            }
+          ],
+          multiMonthMinWidth: 200,
+          multiMonthMaxColumns: 4,
+          contentHeight:"auto",
+          dayHeaderFormat: {
+            weekday: 'narrow',    // Monday, Wednesday, etc
+          },
+          eventClick: function(date, jsEvent, view) {
+            console.log("hi");
+          },
+          defaultAllDay:true,
+          navLinks: true
         })
 
         calendar.render();
